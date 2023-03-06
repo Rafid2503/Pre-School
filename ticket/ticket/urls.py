6ea589b7ab542cd6e025cece4b881ec1ffeea388
Rafid2503 <mohammadrafid2503@gmail.com>
2023-03-06 19:24:58 +0600

    Initial commit

---
+++ ticket/ticket/urls.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .import views,admin1,staff,student
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('base/',views.BASE,name='base'),
+
+#login path
+    path('',views.LOGIN,name='login'),
+    path('doLogin',views.doLogin,name='doLogin'),
+    path('doLogout',views.doLogout,name='logout'),
+
+#profile
+    path('profile', views.PROFILE, name='profile'),
+    path('profile/update', views.PROFILE_UPDATE, name='profile_update'),
+
+# Admin panel
+    path('admin1/home',admin1.HOME,name='admin_home'),
+    path('admin1/student/add',admin1.ADD_STUDENT,name='add_student'),
+
+
+#Student Panel
+    path('student/home',student.HOME,name='student_home'),
+
+
+
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
