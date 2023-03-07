33d7e7c156efb9e326810a861a1281b64ad00cc8
Rafid2503 <mohammadrafid2503@gmail.com>
2023-03-07 12:32:12 +0600

    Revert Add student panel

--- ticket/ticket/urls.py
+++ ticket/ticket/urls.py
@@ -23,20 +23,13 @@ urlpatterns = [
     path('admin1/home',admin1.HOME,name='admin_home'),
     path('admin1/student/add',admin1.ADD_STUDENT,name='add_student'),
     path('admin1/student/view',admin1.VIEW_STUDENT,name='view_student'),
-<<<<<<< HEAD
     path('Admin1/Subject/Edit/<str:id>',admin1.EDIT_SUBJECT,name='edit_subject'),
     path('Admin1/Subject/Add',admin1.ADD_SUBJECT,name='add_subject'),
-
-=======
     path('admin1/student/edit/<str:id>',admin1.EDIT_STUDENT,name='edit_student'),
     path('admin1/student/update',admin1.UPDATE_STUDENT,name='update_student'),
-<<<<<<< HEAD
     path('admin1/student/delete<str:admin>',admin1.DELETE_STUDENT,name='delete_student'),
 
     path('admin1/course/add',admin1.ADD_COURSE,name='add_course'),
-=======
->>>>>>> 61877704f2050b965424f54b436870af8d1b150f
->>>>>>> 5e89bfc3b4a5ad4af07063ef7aa54e0aad831133
 
 #Student Panel
     path('student/home',student.HOME,name='student_home'),
