1ce0c279e7062ad1c7027f1088b2bb45c6a198e0
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2023-03-23 02:48:50 +0600

    Database (Updated)

--- ticket/ticket/urls.py
+++ ticket/ticket/urls.py
@@ -64,9 +64,11 @@ urlpatterns = [
     path('admin1/student/feedback',admin1.STUDENT_FEEDBACK, name='get_student_feedback'),
     path('admin1/student/feedback/reply/save',admin1.REPLY_STUDENT_FEEDBACK, name='reply_student_feedback'),
 
-    path('student/apply_for_leave', student.STUDENT_LEAVE, name='student_leave'),
-    path('student/Leave_save', student.STUDENT_LEAVE_SAVE, name='student_leave_save'),
+
     path('admin1/student/Leave_view', admin1.STUDENT_LEAVE_VIEW, name='student_leave_view'),
+
+    path('Admin1/Student/approve_leave/<str:id>', admin1.STUDENT_APPROVE_LEAVE,name='student_approve_leave'),
+    path('Admin1/Student/disapprove_leave/<str:id>', admin1.STUDENT_DISAPPROVE_LEAVE,name='student_disapprove_leave'),
 
 
 
@@ -97,9 +99,9 @@ urlpatterns = [
     path('student/mark_as_done/<str:status>', student.STAFF_NOTIFICATION_MARK_AS_DONE, name='student_notifications_mark_as_done'),
     path('student/feedback',student.STUDENT_FEEDBACK, name='student_feedback'),
     path('student/feedback/save',student.STUDENT_FEEDBACK_SAVE, name='student_feedback_save'),
+    path('student/apply_for_leave', student.STUDENT_LEAVE, name='student_leave'),
+    path('student/Leave_save', student.STUDENT_LEAVE_SAVE, name='student_leave_save'),
 
-    path('Admin1/Student/approve_leave/<str:id>', admin1.STUDENT_APPROVE_LEAVE, name='student_approve_leave'),
-    path('Admin1/Student/disapprove_leave/<str:id>', admin1.STUDENT_DISAPPROVE_LEAVE,name='student_disapprove_leave'),
 
 #staff notification
     path('staff/home', staff.HOME, name='staff_home'),
